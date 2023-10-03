0c8af2759cbf1d2b26f00c480cadff1d12e967ce
Jafarscript <lihammedjafar@gmail.com>
2023-10-03 23:56:54 +0100

    C# progession

--- Program.cs
+++ Program.cs
@@ -1,3 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string kolade = "Jafar";
-Console.WriteLine($"Hello{kolade}" );
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+
+Console.WriteLine($"{kolade}, this is the range of decimal in CSharp {min} and {max}");
+
+decimal a = 1.5M;
+decimal b = 3.5M;
+
+Console.WriteLine(a/b);
+
+double d = 1.5;
+double c = 3.5;
+
+Console.WriteLine(c/d);
+
+// Area of a circle
+
+
+double radius = 2.0;
+double area = Math.PI * radius * radius;
+
+Console.WriteLine($"The area of a circle with the radius of {radius} equals to {area}");
+
+
+// Working with strings
+
+// Replace(1,2) just line in python
+// Contains(1)
+//StartsWith()
+// EndsWith()
+
+string name = "    Jafar    ";
+string newName = name.Trim().Replace("Jafar", "Anas");
+name = name.Trim();
+Console.WriteLine(newName);
+Console.WriteLine(name.Length);
+Console.WriteLine(name.ToUpper());
+Console.WriteLine(name.ToLower());
+
